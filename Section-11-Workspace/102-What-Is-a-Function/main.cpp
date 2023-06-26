4ed7f348751f2df7bc76687d77fe4e2756ab5950
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-06-26 06:41:23 -0500

    Adding 102-What-Is-a-Function files and directories

---
+++ Section-11-Workspace/102-What-Is-a-Function/main.cpp
@@ -0,0 +1,38 @@
+/*
+ *  Author: Matt Johnson
+ *  Subject: functions
+ *  Purpose: explore sqrt and pow functions
+ *  Date: 06/26/2023
+ */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+    
+    double num {};
+    
+    cout << "Enter a number (double): ";
+    cin >> num;
+    
+    cout << "The sqrt of " << num << " is: " << sqrt(num) << endl;
+    cout << "The cubed root of " << num << " is: " << cbrt(num) << endl;
+    
+    cout << "\nThe sine of " << num << " is: " << sin(num) << endl;
+    cout << "The cosine of " << num << " is: " << cos(num) << endl;
+    
+    cout << "\nThe ceil of " << num << " is: " << ceil(num) << endl;
+    cout << "The floor of " << num << " is: " << floor(num) << endl;
+    
+    double power {};
+    
+    cout << "\nEnter a power to raise " << num << " to: ";
+    cin >> power;
+    
+    cout << num << " raised to the " << power << " power is: " << pow(num, power) << endl;
+    
+    cout << endl;
+    return 0;
+}
