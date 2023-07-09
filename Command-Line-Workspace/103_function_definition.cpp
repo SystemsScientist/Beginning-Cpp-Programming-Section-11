c7355c8b00e6965e2ee3aa324143b551c93abca7
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-07-09 06:08:53 -0500

    Adding 103_function_definition.cpp file

---
+++ Command-Line-Workspace/103_function_definition.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+const double pi {3.14159};
+
+double calculate_area_circle (double, radius) {
+	return pi * radius * radius;
+}
+
+double calculate_volume_cylinder (double radius, double height) {
+
+
+
