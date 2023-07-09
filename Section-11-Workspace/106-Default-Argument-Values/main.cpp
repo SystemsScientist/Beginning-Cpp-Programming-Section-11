8772951bda77f7f4fa885fa3f3db2d15c77d7999
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-07-09 06:05:34 -0500

    Adding 106-Default-Argument-Values files and directories

---
+++ Section-11-Workspace/106-Default-Argument-Values/main.cpp
@@ -0,0 +1,56 @@
+/*
+ *  Author: Matt Johnson
+ *  Subject: default argument values
+ *  Purpose: 
+ *  Date: 07/09/2023
+ * 
+ *  Notes:
+ * 
+ *          
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+double calc_cost (double base_cost = 100.0, double tax_rate = 0.06, double shipping = 3.50);
+void greeting (string name, string prefix = "Mr.", string suffix = "");
+
+int main(void) {
+    
+    double cost {0};
+    
+    cost = calc_cost(100.0, 0.08, 4.25);    // will use no defaults --> 100, 0.08, 4.25
+    
+    cout << fixed << setprecision(2);
+    cout << "Cost is: " << cost << endl;    // output is 112.25
+    
+    cost = calc_cost(100.0, 0.08);              // will use default shipping --> 100, 0.08, 3.50
+    cout << "Cost is: " << cost << endl;    // output is 111.50
+    
+    cost = calc_cost(200.0);                          // will use default tax and shipping --> 200, 0.06, 3.50 
+    cout << "Cost is: " << cost << endl;    // output is 215.50
+    
+    cost = calc_cost();
+    cout << "Cost is: " << cost << endl;
+    
+    cout << endl;
+    greeting("Glenn Jones", "Dr.", "M.D.");
+    greeting("James Rogers", "Professor", "Ph.D.");
+    greeting("Frank Miller", "Dr.");
+    greeting("William Smith");
+    greeting("Mary Howard", "Dr.", "Ph.D.");
+    
+    cout << endl;
+    return 0;
+}
+
+double calc_cost (double base_cost, double tax_rate, double shipping) {
+    return base_cost += (base_cost * tax_rate) + shipping;
+}
+
+void greeting(string name, string prefix, string suffix) {
+    cout << "Hello, " << prefix + " " + name + " " + suffix << endl;
+}
